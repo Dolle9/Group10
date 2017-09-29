c03e2f6854d1e34c5b3b96043c9bf6305ce36d97
Dani_ <Dani_@DESKTOP-V8IQED7>
2017-09-29 09:54:49 +0300

    Testiiiii

---
+++ ProjektiD/src/projektid/ProjektiD.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projektid;
+
+/**
+ *
+ * @author Dani_
+ */
+public class ProjektiD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
